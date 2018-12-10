7ad839e51952c863680f07d0f55a4d465e44708c
Pier <pierlu.aba@gmail.com>
2018-12-10 09:45:14 +0100

    Test Commit

---
+++ app/src/main/java/com/example/angela/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.angela.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.i("MainActivity","activity created");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity","activity created");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
