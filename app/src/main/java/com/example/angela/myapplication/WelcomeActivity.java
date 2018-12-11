687c20019866f74e8a6597e47eb727a3073af467
Pier <pierlu.aba@gmail.com>
2018-12-11 21:31:47 +0100

    add client email onclick

--- app/src/main/java/com/example/angela/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/angela/myapplication/WelcomeActivity.java
@@ -1,19 +1,28 @@
 package com.example.angela.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.view.View;
 import android.widget.TextView;
 
-public class WelcomeActivity  extends AppCompatActivity {
+public class WelcomeActivity  extends AppCompatActivity implements View.OnClickListener {
     TextView welcomeET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_welcome);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         welcomeET=findViewById(R.id.welcome_et);
         welcomeET.setText(getIntent().getStringExtra("welcome"));
 
+
+    }
+    public void onClick(View view){
+        Intent i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto",getIntent().getStringExtra("welcome"), null));
+        startActivity(Intent.createChooser(i, "Choose an Email client :"));
     }
 }
