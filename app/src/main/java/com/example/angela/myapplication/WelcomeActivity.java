9fa0bfa6a9f5f1d21ac21c3af5517aea3b006062
Pier <pierlu.aba@gmail.com>
2018-12-13 12:09:07 +0100

    add external client

--- app/src/main/java/com/example/angela/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/angela/myapplication/WelcomeActivity.java
@@ -10,14 +10,26 @@ import android.widget.TextView;
 public class WelcomeActivity  extends AppCompatActivity implements View.OnClickListener {
     TextView welcomeET;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_welcome);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
         welcomeET=findViewById(R.id.welcome_et);
+
+
+
+    if(getIntent().getStringExtra("welcome")!=null){
+
         welcomeET.setText(getIntent().getStringExtra("welcome"));
 
+    }else {
+        Intent openedBy = getIntent();
+        String openedEmail="";
+        openedEmail=openedBy.getData().toString().substring(7);
+        openedEmail=Uri.decode(openedEmail);
+        welcomeET.setText(openedEmail);
+    }
 
     }
     public void onClick(View view){
